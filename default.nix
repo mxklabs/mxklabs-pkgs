b7bb20c97791d47aa22af46a4580f11303583395
Mark Kattenbelt <mark.kattenbelt@gmail.com>
2021-10-17 21:21:44 +0100

    Updated channel to use github.

--- default.nix
+++ default.nix
@@ -1,52 +1,39 @@
 let
-  #nixpkgs-sources = import ./nix/sources.nix;
   nixpkgs = import <nixpkgs> {};
-
-  #mxklabs-example-cpp-app = import ./mxklabs/mxklabs-example-cpp-app;
-
-
-
-  #hello = pkgs.writeShellScriptBin "hello" ''
-  #  echo "Hello from the Nix channel overlay!"
-  #'';
-
-  #nixpkgs = import nixpkgs-sources.nixpkgs;
 
   mxklabs-example-cpp-app = nixpkgs.stdenv.mkDerivation rec {
     name = "mxklabs-example-cpp-app";
-    src = builtins.fetchGit {
+    src = #../mxklabs-example-cpp-app;
+    builtins.fetchGit {
       url = "git@github.com:mxklabs/mxklabs-example-cpp-app.git";
     };
 
-    baseInputs = [
+    nativeBuildInputs = [
+      nixpkgs.cmake
     ];
 
     buildInputs = [
+      mxklabs-example-cpp-lib1
+    ];
+  };
+
+  mxklabs-example-cpp-lib1 = nixpkgs.stdenv.mkDerivation rec {
+    name = "mxklabs-example-cpp-lib1";
+    src = #../mxklabs-example-cpp-lib1;
+    builtins.fetchGit {
+     url = "git@github.com:mxklabs/mxklabs-example-cpp-lib1.git";
+    };
+
+    nativeBuildInputs = [
       nixpkgs.cmake
     ];
   };
 
   mxklabs-pkgs = {
     mxklabs-example-cpp-app = mxklabs-example-cpp-app;
+    mxklabs-example-cpp-lib1 = mxklabs-example-cpp-lib1;
   };
-
-  #mxklabs-pkgs =
-    
-      #mxklabs-example-cpp-app = mxklabs-example-cpp-app {pkgs=nixpkgs; #.nixpkgs;
-      #};
-    #};
-
-    #[ mxklabs-example-cpp-app ]
-    # {
-    #  overlays = [
-    #    (self: super: {
-    #      inherit hello;
-    #    })
-    #  ];
-    #}
 
 in
   mxklabs-pkgs
-#mxklabs-example-cpp-app 
-#mxklabs-pkgs
 
