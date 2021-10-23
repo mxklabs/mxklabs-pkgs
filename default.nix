6eeea997fa0e3498bfa3e1946fc1993b66c7d018
Mark Kattenbelt <mark.kattenbelt@gmail.com>
2021-10-23 14:00:11 +0100

    Made channel work.

--- default.nix
+++ default.nix
@@ -1,39 +1,6 @@
+{ overlays ? [] }:
 let
-  nixpkgs = import <nixpkgs> {};
-
-  mxklabs-example-cpp-app = nixpkgs.stdenv.mkDerivation rec {
-    name = "mxklabs-example-cpp-app";
-    src = #../mxklabs-example-cpp-app;
-    builtins.fetchGit {
-      url = "git@github.com:mxklabs/mxklabs-example-cpp-app.git";
-    };
-
-    nativeBuildInputs = [
-      nixpkgs.cmake
-    ];
-
-    buildInputs = [
-      mxklabs-example-cpp-lib1
-    ];
-  };
-
-  mxklabs-example-cpp-lib1 = nixpkgs.stdenv.mkDerivation rec {
-    name = "mxklabs-example-cpp-lib1";
-    src = #../mxklabs-example-cpp-lib1;
-    builtins.fetchGit {
-     url = "git@github.com:mxklabs/mxklabs-example-cpp-lib1.git";
-    };
-
-    nativeBuildInputs = [
-      nixpkgs.cmake
-    ];
-  };
-
-  mxklabs-pkgs = {
-    mxklabs-example-cpp-app = mxklabs-example-cpp-app;
-    mxklabs-example-cpp-lib1 = mxklabs-example-cpp-lib1;
-  };
-
+  nixpkgs = import <nixpkgs> { overlays = [ (import ./overlay.nix) ] ++ overlays; };
 in
-  mxklabs-pkgs
+  nixpkgs
 
---
+++ overlay.nix
@@ -0,0 +1,30 @@
+final: prev:
+{
+  mxklabs-example-cpp-app = final.stdenv.mkDerivation rec {
+    name = "mxklabs-example-cpp-app";
+    src = #../mxklabs-example-cpp-app;
+    builtins.fetchGit {
+      url = "git@github.com:mxklabs/mxklabs-example-cpp-app.git";
+    };
+
+    nativeBuildInputs = [
+      final.cmake
+    ];
+
+    buildInputs = [
+      final.mxklabs-example-cpp-lib1
+    ];
+  };
+
+  mxklabs-example-cpp-lib1 = final.stdenv.mkDerivation rec {
+    name = "mxklabs-example-cpp-lib1";
+    src = #../mxklabs-example-cpp-lib1;
+    builtins.fetchGit {
+     url = "git@github.com:mxklabs/mxklabs-example-cpp-lib1.git";
+    };
+
+    nativeBuildInputs = [
+      final.cmake
+    ];
+  };
+}
